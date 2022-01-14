dcf2a84be5441907bae5b4e4b70573a34104a97a
Matthew Nguyen <contact@n-studio.fr>
2022-01-14 23:31:54 +0800

    assets

--- app/assets/builds/administrate/application.js
+++ app/assets/builds/administrate/application.js
@@ -20737,9 +20737,7 @@
   var import_jquery2 = __toESM(require_jquery());
   var import_moment = __toESM(require_moment());
   var import_bootstrap_datetimepicker_min = __toESM(require_bootstrap_datetimepicker_min());
-  (function($5) {
-    $5.fn.datetimepicker = import_bootstrap_datetimepicker_min.default;
-  })(import_jquery2.default);
+  import_jquery2.default.fn.datetimepicker = import_bootstrap_datetimepicker_min.default;
   (0, import_jquery2.default)(function() {
     (0, import_jquery2.default)('[data-type="time"]').datetimepicker({
       debug: false,
